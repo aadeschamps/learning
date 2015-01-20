c4bbc8efcccb75220e5051a2a7ea3b2b05d170da
Alexander Deschamps <alexadeschamps@gmail.com>
2015-01-20 16:10:07 -0500

    added in last lessons of the day

---
+++ Code/w08/d01/ajax/express/examples.js
@@ -0,0 +1,10 @@
+var request = require('request');
+var url = "http://api.randomuser.me/";
+request(url, function(error, response, body){
+	if(!error && response.statusCode == 200){
+		data = JSON.parse(body);
+		var name = data.results[0].user.name.first;
+		var email = data.results[0].user.email;
+		console.log(name + ": " + email);
+	}	
+})
